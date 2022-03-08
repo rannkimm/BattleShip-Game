b069eb05fd2f54dbbd35ad8968f1015787fe821b
Ran Kim <rannio@gmail.com>
2022-03-08 12:29:33 -0500

    changed method of inserting ships to grid array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 ///////// GLOBAL VARIABLES /////////
 let board = document.querySelector('.board')
-board = [
+let cell = document.querySelectorAll('.cell')
+
+let cellValue = [
     0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,
@@ -13,20 +15,40 @@ board = [
     0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,
 ]
+
 let destroyer = ['d', 'd']
 let submarine = ['s', 's', 's']
 let cruiser = ['c', 'c', 'c', 'c']
 let battleship = ['b', 'b', 'b', 'b', 'b']
 let carrier = ['a', 'a', 'a', 'a', 'a', 'a']
 
-createBoard = (name) => {
-    name.splice(1, 2, destroyer[0], destroyer[1])
-    name.splice(25, 3, submarine[0], submarine[1], submarine[2],)
-    name.splice(66, 4, cruiser[0], cruiser[1], cruiser[2], cruiser[3])
-    name.splice(92, 5, battleship[0], battleship[1], battleship[2], battleship[3], battleship[4])
-    name.splice(48, 6, carrier[0], carrier[1], carrier[2], carrier[3], carrier[4], carrier[5])
+createBoard = () => {
+    cellValue.splice(1, 2, destroyer[0], destroyer[1])
+    cellValue.splice(25, 3, submarine[0], submarine[1], submarine[2],)
+    cellValue.splice(66, 4, cruiser[0], cruiser[1], cruiser[2], cruiser[3])
+    cellValue.splice(92, 5, battleship[0], battleship[1], battleship[2], battleship[3], battleship[4])
+    cellValue.splice(48, 6, carrier[0], carrier[1], carrier[2], carrier[3], carrier[4], carrier[5])
+    setValue = () => {
+        cell.innerHTML = cellValue
+    }
+    setValue()
+
+    start = () => {
+        for (let i = 0; i < cell.length; i++) {
+            cell[i].addEventListener('click', () => {
+                if (cell[i].value === 0) {
+                    console.log('miss')
+                } else {
+                    console.log('hit')
+                }
+            }, {once: true})
+        }
+    }
 }
 
 
 
-createBoard(board)
+
+
+createBoard()
+start()
